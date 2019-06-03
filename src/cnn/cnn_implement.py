4c85607f620339c1387a66be74df0e9682e7bd09
Panagiotis Soilis <pan.soilis@gmail.com>
2019-06-03 12:34:36 +0200

    Stratify implementation

--- src/cnn/cnn_implement.py
+++ src/cnn/cnn_implement.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
-# import time
 import pickle
 from torch.optim.lr_scheduler import StepLR
 from src import filters
@@ -16,7 +15,7 @@ class SimpleCNN(nn.Module):
 
 		#Input channels 3 output channels 30
 		self.conv1 = nn.Conv2d(3, 30, kernel_size=5, stride=1, padding=0)
-		self.conv1.weight = nn.Parameter(filters.get_filters())
+		# self.conv1.weight = nn.Parameter(filters.get_filters()) - used with SRM filters
 		self.conv2 = nn.Conv2d(30, 30, kernel_size=5, stride=2, padding=0)
 		self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 		self.conv3 = nn.Conv2d(30, 16, kernel_size=3, stride=1, padding=0)
@@ -29,11 +28,12 @@ class SimpleCNN(nn.Module):
 		self.conv9 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=0)
 		self.fc1 = nn.Linear(16*5*5, 1000)
 		self.drop1 = nn.Dropout(p=0.5)
-		self.fc2 = nn.Linear(1000,2)
+		self.fc2 = nn.Linear(1000, 2)
 		
 	def forward(self,x):
 		x = x.unsqueeze(0)
-		x = F.relu(self.conv1(x))
+		# x = F.relu(self.conv1(x)) - used with SRM filters
+		x = F.relu(nn.init.xavier_uniform_(self.conv1(x)))
 		x = F.relu(nn.init.xavier_uniform_(self.conv2(x)))
 		lrn = nn.LocalResponseNorm(2)
 		x = lrn(x)
@@ -67,46 +67,43 @@ def createLossandOptimizer(net, learning_rate=0.01):
 
 
 def trainNet(net, train_set, n_epochs, learning_rate):
-
-	print("===Hyperparameters===")
-	print("epochs=", n_epochs)
-	print("learning_rate=", learning_rate)
-
 	loss, optimizer = createLossandOptimizer(net, learning_rate)
-	# training_start_time = time.time()
-	scheduler = StepLR(optimizer, step_size=10, gamma=0.9)
-
+	scheduler = StepLR(optimizer, step_size=10*len(train_set), gamma=0.9)
 	for epoch in range(n_epochs):
 		running_loss = 0.0
-		# start_time = time.time()
-		# total_train_loss = 0.0
-		print('Epoch: ', epoch)
+		print('Epoch: ', epoch + 1)
 		print(scheduler.get_lr())
+		print(optimizer)
+		# loop over the training samples
 		for i, data in enumerate(train_set, 0):
+			# get the inputs
 			inputs, labels = data
-			# Backprop and perform Adam optimisation
+			if torch.cuda.is_available():
+				inputs = inputs.cuda()
+				labels = labels.cuda()
+			# zero the parameter gradients
 			optimizer.zero_grad()
+			# forward + backward + optimize
 			outputs = net(inputs)
 			labels = [labels]
 			labels = torch.from_numpy(np.array(labels))
 			loss_size = loss(outputs, labels)
 			loss_size.backward()
+			# TODO: check why on the 10th epoch it multiplies with an extra 0.9
+			scheduler.step()
 			# optimizer.step()
 			# Print statistics
 			running_loss += loss_size.item()
 			if i % 2000 == 1999:  # print every 2000 mini-batches
 				print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
 				running_loss = 0.0
-		# TODO: check why on the 10th epoch it multiplies with an extra 0.9
-		scheduler.step()
 
 	print('Finished Training')
 
 
 def main():
 	dataset = pickle.load(open("dataset.pickle", "rb"))
-	# TODO: stratified train test split
-	train, test = train_test_split(dataset, test_size=0.2, random_state=0)
+	train, test = train_test_split(dataset, test_size=0.2, random_state=0, stratify=dataset.targets)
 	pickle_out = open("test.pickle", "wb")
 	pickle.dump(test, pickle_out)
 	pickle_out.close()
