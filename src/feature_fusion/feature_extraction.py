59a184f3478c011f9a681901dc6e5b6dd6278894
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2019-06-03 12:27:40 +0200

    Initial implementation of patch extraction for feature fusion

--- src/feature_fusion/feature_extraction.py
+++ src/feature_fusion/feature_extraction.py
@@ -1,2 +1,31 @@
-def get_patch():
-    return None
+import glob
+
+import cv2
+from skimage.util import view_as_windows
+
+
+def get_images_and_labels():
+    tampered_dir = '../../data/CASIA2/Tp/*'
+    authentic_dir = '../../data/CASIA2/Au/*'
+    images = {}
+    for im in glob.glob(authentic_dir):
+        images[im] = {}
+        images[im]['mat'] = cv2.imread(im)
+        images[im]['label'] = 0
+    for im in glob.glob(tampered_dir):
+        images[im] = {}
+        images[im]['mat'] = cv2.imread(im)
+        images[im]['label'] = 1
+    return images
+
+
+def get_patches(image_mat, stride):
+    window_shape = (128, 128, 3)
+    windows = view_as_windows(image_mat, window_shape, step=stride)
+    patches = []
+    for m in range(windows.shape[0]):
+        for n in range(windows.shape[1]):
+            patches += [windows[m][n][0]]
+    return patches
+
+
--- src/feature_fusion/svm_feature_generation.py
+++ src/feature_fusion/svm_feature_generation.py
@@ -1,18 +1,28 @@
+import time
+
+from src.feature_fusion.feature_extraction import get_images_and_labels, get_patches
+
+
 def create_SVM_features():
-
     # for every image (RAFAIL)
-
+    images = get_images_and_labels()
+    for image_name in images.keys():
         # get name and label (RAFAIL)
+        image = images[image_name]['mat']
+        label = images[image_name]['label']
 
-        # generate pathes (RAFAIL)
+        # generate patches (RAFAIL)
+        patches = get_patches(image, stride=128)
 
         # init Y (KYRIAKOS)
 
         # for every patch (RAFAIL)
+        for patch in patches:
+            pass
 
-            # call CNN -> Yi (KYRIAKOS)
+        # call CNN -> Yi (KYRIAKOS)
 
-            # append Yi to Y (KYRIAKOS)
+        # append Yi to Y (KYRIAKOS)
 
         # create Y_hat with mean or max (KYRIAKOS)
 
@@ -21,9 +31,5 @@ def create_SVM_features():
         return None
 
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    create_SVM_features()
