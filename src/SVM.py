d10eb8c7c475a01dc80ccba09b7bdb716775d7ba
Panagiotis Soilis <pan.soilis@gmail.com>
2019-06-19 23:08:50 +0200

    SVM update

--- src/SVM.py
+++ src/SVM.py
@@ -1,53 +1,29 @@
-import pandas as pd
-import csv
 from sklearn import svm
-import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
-print('start------------------------------------------------')
-dataset = pd.read_csv(header=None,filepath_or_buffer="dummy_feature_export.csv")
-labels = dataset[1].tolist()
-dataset = dataset.drop([0,1],axis=1)
-list_set = dataset.values.tolist()
-svc = svm.SVC(gamma="scale")
-print(svc.get_params())
-tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
-					'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}]
-scores = ['precision', 'recall']
-for score in scores:
-	print("# Tuning hyper-parameters for %s" % score)
-	print()
 
-	clf = GridSearchCV(svc, tuned_parameters, cv=2,
-									scoring='%s_macro' % score)
-	clf.fit(list_set,labels)
+# Read features and labels from CSV
+df = pd.read_csv(filepath_or_buffer="svm_features.csv")
+X = df.loc[:, ~df.columns.isin(['labels', 'image_names'])]
+y = df['labels']
 
-	print("Best parameters set found on development set:")
-	print()
-	print(clf.best_params_)
-	print()
-	print("Grid scores on development set:")
-	print()
-	means = clf.cv_results_['mean_test_score']
-	stds = clf.cv_results_['std_test_score']
-	for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-	        print("%0.3f (+/-%0.03f) for %r"
-	              % (mean, std * 2, params))
-	print()
+print(df.isnull().values.any())
 
-	print("Detailed classification report:")
-	print()
-	print("The model is trained on the full development set.")
-	print("The scores are computed on the full evaluation set.")
-	print()
-	y_true, y_pred = labels, clf.predict(list_set)
-	print(classification_report(y_true, y_pred))
-	print()
+# Optimize hyper-parameters
+# hyper_params = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+# 					'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}]
+# model = svm.SVC()
+# model_grid_search = GridSearchCV(model, hyper_params, cv=10, iid=False)
+# model_grid_search.fit(X.values, y.values)
+# print("Optimal hyper-parameters: ", model_grid_search.best_params_)
+# print("Accuracy :", model_grid_search.best_score_)
+# model = svm.SVC(model_grid_search.best_params_)
 
-
-
-
-
-
-
+# Single SVM run
+model = svm.SVC(kernel='rbf', gamma=1e-3, C=0.01)
+scores = cross_val_score(model, X, y, cv=10, scoring='accuracy', n_jobs=-1)
+print(scores)
+print(np.mean(scores))
+print(np.std(scores))
