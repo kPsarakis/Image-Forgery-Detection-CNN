a515fb4428e937d5ca08d3c7655a2ecf32ef35d2
Panagiotis Soilis <pan.soilis@gmail.com>
2019-06-24 13:46:09 +0200

    Graph legend update

--- src/plots_graphs.py
+++ src/plots_graphs.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def plot_epochs(metric1, metric2, ylab):
-    plt.plot(metric1, label='CAISA2')
+    plt.plot(metric1, label='CASIA2')
     plt.plot(metric2, label='NC16')
     plt.ylabel(ylab)
     plt.xlabel("Epoch")
     plt.legend(loc='lower right')
     plt.show()
+
 
 if __name__ == "__main__":
     df1 = pd.read_csv(filepath_or_buffer="../data/output/SRM_accuracy_Full_NoRot_LR0005_b200_nodrop.csv")
