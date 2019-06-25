03aa40fe11018d690da0fa3569576d56d4d0cd8a
Panagiotis Soilis <pan.soilis@gmail.com>
2019-06-25 16:22:00 +0200

    Print misclassified

--- src/classification/SVM.py
+++ src/classification/SVM.py
@@ -44,3 +44,18 @@ def print_confusion_matrix(X, y, opt_params):
     df = pd.DataFrame(data, columns=['y_Actual', 'y_Predicted'])
     conf_matrix = pd.crosstab(df['y_Actual'], df['y_Predicted'], rownames=['Actual'], colnames=['Predicted'])
     sn.heatmap(conf_matrix, cmap=ListedColormap(['#ED7D31', '#009FDA']), annot=True, fmt='g', cbar=False)
+
+
+def find_misclassified(X, y, opt_params, img_ids):
+    # Run one SVM with 80-20 split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=0)
+    model = svm.SVC(kernel='rbf', gamma=opt_params['gamma'], C=opt_params['C'])
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    misclassified = []
+    for i in range(len(y_test)):
+        if y_pred[i] != y_test.values[i]:
+            misclassified.append(str(y_pred[i]) + ',' + str(y_test.values[i]) + ',' + str(img_ids[y_test.index[i]]))
+    df = pd.DataFrame(misclassified)
+    df.columns = ['Prediction,Actual,ImageName']
+    df.to_csv('Misclassified.csv', index=False)
--- src/svm_classification.py
+++ src/svm_classification.py
@@ -1,11 +1,13 @@
 import pandas as pd
-from classification.SVM import optimize_hyperparams, classify, print_confusion_matrix
+from classification.SVM import optimize_hyperparams, classify, print_confusion_matrix, find_misclassified
 
 # Read features and labels from CSV
 df = pd.read_csv(filepath_or_buffer='../data/output/features/CASIA2_WithRot_LR001_b128_nodrop.csv')
 X = df.loc[:, ~df.columns.isin(['labels', 'image_names'])]
 y = df['labels']
+img_ids = df['image_names']
 print('Has NaN:', df.isnull().values.any())
 opt_params = optimize_hyperparams(X, y)
 classify(X, y, opt_params)
 print_confusion_matrix(X, y, opt_params)
+find_misclassified(X, y, opt_params, img_ids)
