b4a0f78708152475de127839ffe42f53bd9ffad4
Kyriakos Psarakis <kpsarakis94@gmail.com>
2019-06-25 23:57:21 +0200

    duplicate code removal

--- src/feature_fusion/feature_vector_generation.py
+++ src/feature_fusion/feature_vector_generation.py
@@ -9,31 +9,18 @@ from skimage import io
 
 
 def create_feature_vectors(model, tampered_path, authentic_path):
-    transform = transforms.Compose([transforms.ToTensor()])
+
     df = pd.DataFrame()
     images = get_images_and_labels(tampered_path, authentic_path)
     c = 1
     for image_name in images.keys():  # images
         print("Image: ", c)
+
         image = images[image_name]['mat']
         label = images[image_name]['label']
-        y = []  # init Y
 
-        patches = get_patches(image, stride=128)
-
-        for patch in patches:  # for every patch
-            img_tensor = transform(patch)
-            img_tensor.unsqueeze_(0)
-            img_variable = Variable(img_tensor.double())
-            yi = get_yi(model=model, patch=img_variable)
-            y.append(yi)  # append Yi to Y
-
-        y = np.vstack(tuple(y))
-
-        y_hat = get_y_hat(y=y, operation="mean")  # create Y_hat with mean or max
-
-        df = pd.concat([df, pd.concat([pd.DataFrame([image_name.split("\\")[1], str(label)]), pd.DataFrame(y_hat)])],
-                       axis=1, sort=False)
+        df = pd.concat([df, pd.concat([pd.DataFrame([image_name.split("\\")[1], str(label)]),
+                                       pd.DataFrame(get_patch_yi(model, image))])], axis=1, sort=False)
         c += 1
 
     # save the feature vector to csv
@@ -43,31 +30,18 @@ def create_feature_vectors(model, tampered_path, authentic_path):
 
 
 def create_feature_vectors_nc(model):
-    transform = transforms.Compose([transforms.ToTensor()])
+
     df = pd.DataFrame()
     images = get_images_and_labels_nc()
     c = 1
     for image_name, label in images.items():  # images
+
         print("Image: ", c)
+
         image = io.imread('../../data/NC2016_Test0601/' + image_name)
 
-        y = []  # init Y
-
-        patches = get_patches(image, stride=1024)
-
-        for patch in patches:  # for every patch
-            img_tensor = transform(patch)
-            img_tensor.unsqueeze_(0)
-            img_variable = Variable(img_tensor.double())
-            yi = get_yi(model=model, patch=img_variable)
-            y.append(yi)  # append Yi to Y
-
-        y = np.vstack(tuple(y))
-
-        y_hat = get_y_hat(y=y, operation="mean")  # create Y_hat with mean or max
-
-        df = pd.concat([df, pd.concat([pd.DataFrame([image_name, str(label)]), pd.DataFrame(y_hat)])],
-                       axis=1, sort=False)
+        df = pd.concat([df, pd.concat([pd.DataFrame([image_name, str(label)]),
+                                       pd.DataFrame(get_patch_yi(model, image))])], axis=1, sort=False)
         c += 1
 
     # save the feature vector to csv
@@ -76,6 +50,28 @@ def create_feature_vectors_nc(model):
     final_df.columns = get_df_column_names()
 
     final_df.to_csv('test.csv', index=False)  # csv type [im_name][label][f1,f2,...,fK]
+
+
+def get_patch_yi(model, image):
+
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    y = []  # init Y
+
+    patches = get_patches(image, stride=1024)
+
+    for patch in patches:  # for every patch
+        img_tensor = transform(patch)
+        img_tensor.unsqueeze_(0)
+        img_variable = Variable(img_tensor.double())
+        yi = get_yi(model=model, patch=img_variable)
+        y.append(yi)  # append Yi to Y
+
+    y = np.vstack(tuple(y))
+
+    y_hat = get_y_hat(y=y, operation="mean")  # create Y_hat with mean or max
+
+    return y_hat
 
 
 def get_df_column_names():
